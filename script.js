4fefb6a0f171a12b1e90bba323ac932874d14aed
Chris Johnson <chrisjohnson112@yahoo.com>
2019-12-27 18:55:41 -0600

    init upload

---
+++ script.js
@@ -0,0 +1,38 @@
+// Set the date we're counting down to
+var countDownDate = new Date("Jan 1, 2020 00:00:00").getTime();
+
+
+// Update the count down every 1 second
+var x = setInterval(function() {
+
+  // Get today's date and time
+  var now = new Date().getTime();
+
+  // Find the distance between now and the count down date
+  var distance = countDownDate - now;
+
+  // Time calculations for days, hours, minutes and seconds
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  // Display the result in the element with id="demo"
+document.getElementById("days").innerHTML = days + "<br>" + "Days";
+document.getElementById("hours").innerHTML = hours + "<br>" + "Hours";
+document.getElementById("minutes").innerHTML = minutes  + "<br>" + "Minutes";
+document.getElementById("seconds").innerHTML = seconds  + "<br>" + "Seconds";
+
+
+  /*document.getElementById("clock").innerHTML = days + "d " + hours + "h "
+  + minutes + "m " + seconds + "s ";*/
+
+  // If the count down is finished, write some text
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("days").innerHTML = "HAPPY";
+    document.getElementById("hours").innerHTML = "NEW";
+    document.getElementById("minutes").innerHTML = "YEARS";
+    document.getElementById("seconds").innerHTML = "2020!!!!";
+  }
+}, 1000);
